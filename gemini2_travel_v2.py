1bdb9ef29356e158011249f1d9eec16edf76b749
Anurag <anuragma807@gmail.com>
2025-11-05 15:21:24 +0530

    port chaanged

--- gemini2_travel_v2.py
+++ gemini2_travel_v2.py
@@ -201,4 +201,4 @@ async def ep_general_travel_query(req: GeneralQueryRequest):
 
 # --- Server Execution ---
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8186)
